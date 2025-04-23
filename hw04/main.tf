0edc4bf916fb102abd8b02df053622a39c5e6856
Kairixir <kairixir@protonmail.com>
2025-04-24 00:29:47 +0200

    feat: finish hw04

--- hw04/main.tf
+++ hw04/main.tf
@@ -14,9 +14,35 @@ provider "openstack" {
 
 }
 
-resource "openstack_networking_port_v2" "port_1" {
-  name       = "port_1"
-  network_id = var.network_id
+resource "openstack_networking_port_v2" "vm_port" {
+  network_id         = var.network_id
+  security_group_ids = [openstack_networking_secgroup_v2.secgroup_1.id]
+}
+
+resource "openstack_compute_instance_v2" "vm" {
+  name        = var.vm_name
+  image_id    = var.image_id
+  flavor_name = var.flavor_name
+
+  key_pair = var.key_pair
+
+  security_groups = [openstack_networking_secgroup_v2.secgroup_1.name]
+
+  user_data = <<-EOF
+	#!/bin/bash
+	apt-get update -y
+	apt-get install apache2 -y
+	# Create the index.html file with Name and UCO
+	cat <<EOT > /var/www/html/index.html
+	Name: Sevcik Ondrej, UCO: 485230
+	EOT
+	systemctl restart apache2
+  EOF
+
+  network {
+    port = openstack_networking_port_v2.vm_port.id
+  }
+
 }
 
 resource "openstack_networking_floatingip_v2" "floating_ip" {
@@ -25,8 +51,7 @@ resource "openstack_networking_floatingip_v2" "floating_ip" {
 
 resource "openstack_networking_floatingip_associate_v2" "fip_associate" {
   floating_ip = openstack_networking_floatingip_v2.floating_ip.address
-  port_id     = openstack_networking_port_v2.port_1.id
-  depends_on  = [openstack_compute_instance_v2.vm, openstack_networking_floatingip_v2.floating_ip]
+  port_id     = openstack_networking_port_v2.vm_port.id
 }
 
 
@@ -43,32 +68,6 @@ resource "openstack_networking_secgroup_rule_v2" "secgroup_rule" {
   port_range_max    = 80
   remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = openstack_networking_secgroup_v2.secgroup_1.id
-}
-
-resource "openstack_compute_instance_v2" "vm" {
-  name        = var.vm_name
-  image_id    = var.image_id
-  flavor_name = var.flavor_name
-
-  key_pair = var.key_pair
-
-  security_groups = ["default", openstack_networking_secgroup_v2.secgroup_1.name]
-
-  user_data = <<-EOF
-	#!/bin/bash
-	apt-get update -y
-	apt-get install apache2 -y
-	# Create the index.html file with Name and UCO
-	cat <<EOT > /var/www/html/index.html
-	Name: Sevcik Ondrej, UCO: 485230
-	EOT
-	systemctl restart apache2
-  EOF
-
-  network {
-    port = openstack_networking_port_v2.port_1.id
-  }
-
 }
 
 
